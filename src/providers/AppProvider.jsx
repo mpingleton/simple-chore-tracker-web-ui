61e1fe1032f4d4a6ccc2ad8c55d09a4de8495d48
Michael Pingleton <mnpingleton@gmail.com>
2023-11-12 10:12:25 -0500

    Introduced theming into the app provider.

--- src/providers/AppProvider.jsx
+++ src/providers/AppProvider.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 import PrimaryRouter from '../routers/PrimaryRouter';
 
 function AppProvider() {
+
+    const lightTheme = createTheme({
+        palette: {
+            mode: 'light',
+            background: {
+                default: '#A0A0A0',
+                paper: '#CCCCCC'
+            },
+            primary: {
+                main: '#005500',
+            },
+            secondary: {
+                main: '#000055',
+            },
+        },
+    });
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <PrimaryRouter />
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline />
+                <PrimaryRouter />
+            </ThemeProvider>
         </LocalizationProvider>
     );
 }
